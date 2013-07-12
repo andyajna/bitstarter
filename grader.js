3576c42cacef5c21ac4694622d9d8cb40aa8ed52
Ubuntu <ubuntu@ip-172-31-39-151.us-west-2.compute.internal>
2013-07-12 07:15:28 +0000

    added url functionality to grader.js

--- grader.js
+++ grader.js
@@ -26,6 +26,7 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var restler = require('restler');
 
 var assertFileExists = function(infile) {
 	var instr = infile.toString();
@@ -55,6 +56,29 @@ var checkHtmlFile = function(htmlfile, checksfile) {
 	return out;
 };
 
+var urlResponse = function(url, checks) {
+	restler.get(url).on('complete', function(result) {
+		if (result instanceof Error) {
+			console.log('Error: ' + result.message);
+			this.retry(5000);
+		}
+		else {
+			checkUrlFile(result, checks);
+		}
+	});
+};
+
+var checkUrlFile = function(html, checksfile) {
+	$ = cheerio.load(html);
+	var checks = loadChecks(checksfile).sort();
+	var out = {};
+	for(var ii in checks) {
+		var present = $(checks[ii]).length > 0;
+		out[checks[ii]] = present;
+	}
+	console.log(out);
+};
+
 var clone = function(fn) {
 	// Workaround for commander.js issue.
 	// http://stackoverflow.com/a/6772648
@@ -65,10 +89,17 @@ if(require.main == module) {
 	program
 		.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
 		.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+		.option('-u, --url <url>', 'URL to evaluate')
 		.parse(process.argv);
-	var checkJson = checkHtmlFile(program.file, program.checks);
-	var outJson = JSON.stringify(checkJson, null, 4);
-	console.log(outJson);	
+
+		if(program.url) {
+			urlResponse(program.url, program.checks);
+		}
+		else {
+			var checkJson = checkHtmlFile(program.file, program.checks);
+			var outJson = JSON.stringify(checkJson, null, 4);
+			console.log(outJson);
+		}	
 } else {
 	exports.checkHtmlFILE = checkHtmlFile;
 }
